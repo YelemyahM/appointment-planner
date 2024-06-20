49a7db9a07da018b54e2e7c3559e9df07ed2c086
Yele <yele@MacBook-Pro-de-Yele.local>
2024-06-20 15:18:18 +0200

    feat : implementation of ContactPicker, a drop-down list of all of contact names.

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ contacts, onChange, value, name }) => {
   return (
-    <></>
+    <div>
+      <select
+        aria-label="Contact Picker"
+        onChange={onChange}
+        value={value}
+        name={name}
+      >
+        <option value="" key={-1}>
+          No Contact Selected
+        </option>
+        {name.map((contact) => {
+          return (
+            <option value={contact} key={contact}>
+              {contact}
+            </option>
+          );
+        })}
+      </select>
+    </div>
   );
 };
