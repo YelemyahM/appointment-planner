07b7b70537002acc4cdbd1d7e5a1483bab05d765
Yele <yele@MacBook-Pro-de-Yele.local>
2024-06-20 15:07:43 +0200

    feat : implementation of AppointmentForm component who render a form to collect appointment information

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,15 +1,14 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 const getTodayString = () => {
-  const [month, day, year] = new Date()
-    .toLocaleDateString("en-US")
-    .split("/");
+  const [month, day, year] = new Date().toLocaleDateString("en-US").split("/");
   return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
 };
 
 export const AppointmentForm = ({
   contacts,
-  title,
+  name,
   setTitle,
   contact,
   setContact,
@@ -17,10 +16,54 @@ export const AppointmentForm = ({
   setDate,
   time,
   setTime,
-  handleSubmit
+  handleSubmit,
 }) => {
+  const contactNames = useMemo(() => {
+    return contacts.map((contact) => contact.name);
+  }, [contacts]);
 
   return (
-    <></>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            value={name}
+            name="title"
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            required
+            placeholder="Appointment Name"
+          />
+        </label>
+        <label>
+          <ContactPicker
+            name={contact}
+            value={contact}
+            contacts={contactNames}
+            onChange={(e) => setContact(e.target.value)}
+          />
+        </label>
+        <label>
+          <input
+            value={date}
+            name="date"
+            type="date"
+            min={getTodayString()}
+            onChange={(e) => setDate(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <input
+            value={time}
+            name="time"
+            type="time"
+            onChange={(e) => setTime(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Add</button>
+      </form>
+    </div>
   );
 };
